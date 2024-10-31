e5655447e35ceb9c10a3a693232928cebc0118b5
Rasim Aghayev <rasimaqayev@gmail.com>
2024-11-01 01:40:56 +0400

    feat: Add Api responses list

---
+++ src/be/app/Http/Responses/ApiBaseResponse.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\
+{
+    JsonResponse,
+    Response,
+};
+
+abstract class ApiBaseResponse extends Response implements Responsable
+{
+    /**
+     * @var array|string|object
+     */
+    protected array|string|object $dataOrMessage;
+
+    /**
+     * @var int|null
+     */
+    protected ?int $code;
+
+    /**
+     * @param array|string|object $dataOrMessage
+     * @param int|null $code
+     */
+    private function __construct(array|string|object $dataOrMessage = [], int $code = null)
+    {
+        parent::__construct();
+
+        $this->dataOrMessage = $dataOrMessage;
+        $this->code = $code;
+    }
+
+    /**
+     * @param array|string|object $dataOrMessage
+     * @param int|null $code
+     * @return static
+     */
+    public static function make(array|string|object $dataOrMessage = [], int $code = null): static
+    {
+        return new static($dataOrMessage, $code);
+    }
+
+    /**
+     * @param $request
+     * @return JsonResponse
+     */
+    public function toResponse($request): JsonResponse
+    {
+        $responseArray = [
+            'timestamp' => now()->toIso8601String(),
+            'path' => $request->path(),
+            'method' => $request->method(),
+            'result' => $this->dataOrMessage,
+            'error' => null,
+        ];
+
+        if ($this->code === 422) {
+            $responseArray['error'] = $this->dataOrMessage;
+            $responseArray['result'] = [];
+        }
+
+        $response = new JsonResponse($responseArray);
+
+        $response->setStatusCode($this->code ?? $this->defaultResponseCode());
+        $response->headers->add(
+            $this->headers->all()
+        );
+
+        return $response;
+    }
+
+    /**
+     * @return int
+     */
+    protected function defaultResponseCode(): int
+    {
+        return 500;
+    }
+}
--- src/be/bootstrap/app.php
+++ src/be/bootstrap/app.php
@@ -1,40 +1,79 @@
 <?php
 
+use App\Http\Responses\{
+    ErrorTokenBlacklisted,
+    ErrorTokenInvalid,
+    ErrorTokenExpired,
+    ErrorMethodNotAllowedResponse,
+    ErrorNotFoundResponse,
+    ErrorTooManyAttemptsResponse,
+    ErrorUnauthenticatedResponse,
+    ErrorValidationResponse,
+    ErrorInternalServerErrorResponse
+};
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\{
-    Exceptions,Middleware
+    Exceptions,
+    Middleware
 };
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\{
+    ThrottleRequestsException,
+    HttpResponseException
+};
+use phpOpenSourceSaver\JWTAuth\Exceptions\{
+    TokenExpiredException,
+    TokenInvalidException,
+    TokenBlacklistedException
+};
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
     })
     ->withExceptions(using: function (Exceptions $exceptions) {
-        $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+        $exceptions->renderable(function (Throwable $e, Request $request) {
+
             if ($request->is('api/*') || $request->wantsJson()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Route not found.',
-                    'error' => [
-                        'code' => 404,
-                        'type' => 'NotFoundHttpException',
-                        'details' => config('app.debug')
-                            ? ($e->getMessage() ?: 'The requested endpoint does not exist')
-                            : 'The requested endpoint does not exist'
-                    ],
-                    'timestamp' => now()->toIso8601String(),
-                    'path' => $request->path(),
-                    'method' => $request->method()
-                ], 404);
+                if ($e instanceof HttpResponseException) {
+                    return null;
+                }
+                $response = match (true) {
+                    $e instanceof RouteNotFoundException => ErrorNotFoundResponse::make(),
+                    $e instanceof AuthenticationException => ErrorUnauthenticatedResponse::make(),
+                    $e instanceof BadMethodCallException => ErrorMethodNotAllowedResponse::make(),
+                    $e instanceof ValidationException => ErrorValidationResponse::make($e->errors()),
+                    $e instanceof ThrottleRequestsException => ErrorTooManyAttemptsResponse::make(),
+                    $e instanceof TokenExpiredException => ErrorTokenExpired::make(),
+                    $e instanceof TokenInvalidException => ErrorTokenInvalid::make(),
+                    $e instanceof TokenBlacklistedException => ErrorTokenBlacklisted::make(),
+                    default => null,
+                };
+                if (!$response) {
+                    $errorMessage = config('app.debug') ? $e->getMessage() : '';
+                    $response = ErrorInternalServerErrorResponse::make($errorMessage);
+                }
+                if ($response instanceof Response && $e instanceof HttpException) {
+                    $headers = $e->getHeaders();
+                    $response->headers->add($headers);
+                }
+                return $response;
             }
             return null;
+        });
+
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            return $request->is('api/*') || $request->expectsJson();
         });
     })->create();
