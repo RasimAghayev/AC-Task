ee41478253b19fbe3a713c362816557e9ca235aa
Rasim Aghayev <rasimaqayev@gmail.com>
2024-10-31 10:44:31 +0400

    feat: Add health check route

---
+++ src/be/app/Http/Controllers/Check/CheckController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace app\Http\Controllers\Check;
+
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\{
+    JsonResponse,Response
+};
+use Illuminate\Support\Facades\{
+    Artisan,DB,Http
+};
+
+class CheckController extends Controller
+{
+    /**
+     * @return JsonResponse
+     */
+    public function getDB(): JsonResponse
+    {
+        try {
+            DB::connection()->getPdo();
+            return response()->json(
+                data:['status' => 'Application is up and running, database connection is ok!',],
+                status: Response::HTTP_OK
+            );
+        } catch (Exception $e) {
+            return response()->json(
+                data:['status' => 'Failed to connect to the database.',],
+                status: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function getHealth(): JsonResponse
+    {
+        return response()->json(
+            data:['status' => 'Application is up and running, health is ok!',],
+            status: Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function getStatic(): JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIP(): mixed
+    {
+        return Http::get('https://ipapi.co/json/')->json();
+    }
+
+    public function clearCache(): JsonResponse
+    {
+        Artisan::call('optimize:clear');
+        Artisan::call('config:cache');
+        Artisan::call('config:clear');
+        Artisan::call('clear-compiled');
+        Artisan::call('cache:clear');
+        Artisan::call('route:cache');
+        Artisan::call('view:clear');
+
+        return response()->json([
+            'status' => "Cache is cleared ".date(now()),
+        ]);
+    }
+}
---
+++ src/be/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use app\Http\Controllers\Check\CheckController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['throttle:60,1'])->prefix('v1')->group(function () {
+    Route::group(['prefix' => 'check'], function () {
+        Route::get('db', [CheckController::class, 'getDB']);
+        Route::get('health', [CheckController::class, 'getHealth']);
+        Route::get('static', [CheckController::class, 'getStatic']);
+        Route::get('ip', [CheckController::class, 'getIP']);
+        Route::get('clear_cache', [CheckController::class, 'clearCache']);
+    });
+});
